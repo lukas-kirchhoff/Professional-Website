e7be06f0329100920c2aadaeb94ffcac9631ff8e
Lukas Kirchhoff <lu.kirchhoff@googlemail.com>
2018-10-31 11:05:35 +0100

    updated header

--- index.php
+++ index.php
@@ -11,7 +11,6 @@
     <link rel="stylesheet" type="text/css" href="css/main.css">
     <link rel="stylesheet" type="text/css" href="css/responsive.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css?family=Merriweather|Montserrat" rel="stylesheet">
 </head>
 
 <body>
